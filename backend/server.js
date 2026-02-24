9f97b054165c765cdf65a0f23c7a40106414ae4c
Ilya LIFE <himia744@gmail.com>
2026-02-24 14:54:04 +0300

    feat: init server

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const app = require('./app');
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, (err) => {
+    if (err) {
+        console.error(`Error starting server: ${err}`);
+    } else {
+        console.log(`Server running on port ${PORT}`);
+    }
+});
